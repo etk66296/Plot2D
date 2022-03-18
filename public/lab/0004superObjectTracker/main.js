d4b65a937f578da3efa0b64fb586d522415931da
etk66296 <etk66396@web.de>
2022-03-18 07:29:15 +0100

    tracker system lab, each object is allowed to grap data from another one in an easy way

--- public/lab/0004superObjectTracker/main.js
+++ public/lab/0004superObjectTracker/main.js
@@ -1,170 +1,74 @@
-class RenderSurface extends Any{
+class Plot2DAny {
 
-  constructor(parentHtmlElementId, id) {
+  constructor() {
+
+    this.id = -1
+    this.objectTracker = undefined
+
+  }
+
+}
+
+class ObjectTracker extends Plot2DAny{
+
+  constructor() {
 
     super()
 
-    this.parentHtmlElementId = parentHtmlElementId
-    this.parentHtmlElement = document.getElementById(this.parentHtmlElementId)
-    this.id = id
-    this.width = '800px'
-    this.height = '600px'
-    this.renderingContextType = '2d'
-    this.hasAnAlphaChannel = false
-    this.isDesynchronized = false
-    this.renderingCanvas = undefined
-    this.renderingContext = undefined
+    this.id = 0
+    this.objectTracker = this
+
+    this.objectIdCounter = 0
+    this.objectList = [this]
+    
+  }
+
+  add(any) {
+
+    this.objectIdCounter += 1
+    any.objectTracker = this
+    any.id = this.objectIdCounter
+    this.objectList.push(any)
+
+  }
+
+}
+
+class Sprite extends Plot2DAny {
+  constructor() {
+    super()
+  }
+}
+
+class Plot2D extends Plot2DAny{
+
+  constructor() {
+
+    super()
+    this.objectTracker = undefined 
+
+  }
+
+  getSubscriptionList() {
+
+    return this.objectTracker.objectList
 
   }
 
   init() {
-    
-    this.renderingCanvas = this.createElementNS("canvas")
-    this.renderingCanvas.style.backgroundColor = '#000022'
-    this.renderingCanvas.setAttribute('width', this.width)
-    this.renderingCanvas.setAttribute('height', this.height)
-    this.renderingCanvas.setAttribute('id', this.id)
 
-    if (this.renderingCanvas.getContext) {
+    this.objectTracker = new ObjectTracker()
+    this.subscribe(this)
 
-      this.renderingContext = this.renderingCanvas.getContext(
-        this.renderingContextType, {
-        antialias: this.hasAnAlphaChannel,
-        depth: this.isDesynchronized
-      })
+  }
 
-      this.parentHtmlElement.appendChild(this.renderingCanvas)
+  subscribe(plot2DAny) {
 
-    } else {
-
-      this.parentHtmlElement.style.backgroundColor = '#000000'
-      this.parentHtmlElement.style.color = '#FF00FF'
-      this.parentHtmlElement.style.padding = '10px'
-      this.parentHtmlElement.innerHTML = `<h3>
-        your browser does not support canvas rendering
-      </h3>`
+    for(let i = 0; i < arguments.length; i++) {
       
-    }
-    
-
-  }
-
-  fullscreen() {
-
-    this.renderingCanvas.style.width = '100%'
-    
-    if (this.parentHtmlElement.requestFullscreen) {
-
-      this.parentHtmlElement.requestFullscreen()
-
-    } else if (this.parentHtmlElement.webkitRequestFullscreen) { /* Safari */
-
-    this.parentHtmlElement.webkitRequestFullscreen()
-
-    } else if (this.parentHtmlElement.msRequestFullscreen) { /* IE11 */
-
-    this.parentHtmlElement.msRequestFullscreen()
+      this.objectTracker.add(arguments[i])
 
     }
-  }
-  
-}
-
-class Tracker extends Any {
-  constructor() {
-    super()
-    this.idCounter = 0
-    this.trackedObjects = []
-  }
-
-  registerObject(obj) {
-    this.obj.trackerId = this.idCounter
-    this.trackedObjects.push(obj)
-  }
-  
-}
-
-class DataSymbol extends Any {
-  constructor(parentHtmlElementId, id, data) {
-    super()
-    this.parentHtmlElementId = parentHtmlElementId
-    this.parentHtmlElement = document.getElementById(this.parentHtmlElementId)
-    this.id = id
-    this.data = data
 
   }
-
-  createSprite(imageSheet, cfg) {
-    new Sprite(imageSheet, cfg)
-  }
-}
-
-class JsonSymbol extends DataSymbol {
-  constructor(parentHtmlElementId, id, data) {
-    super(parentHtmlElementId, id, data)
-    this.symbol = this.createElementNS("div")
-    this.symbol.style.backgroundColor = '#ffffff'
-    this.symbol.style.position = 'relative'
-    this.symbol.style.left = '10px'
-    this.symbol.style.top = '10px'
-    this.symbol.style.width = '32px'
-    this.symbol.style.height = '32px'
-    this.symbol.setAttribute('id', this.id)
-    this.symbol.draggable = "true"
-    this.parentHtmlElement.appendChild(this.symbol)
-
-
-    this.symbol.ondrop = (event) => {
-      event.preventDefault()
-      console.log(event.dataTransfer.getData("text"))
-    }
-
-    this.symbol.ondragstart = (event) => {
-      event.dataTransfer.setData("text/plain", 'Hey this is JsonSymbol')
-    }
-
-    this.symbol.ondragover = (event) => {
-      event.preventDefault()
-    }
-    
-  }
-}
-
-class ImageSymbol extends DataSymbol {
-  constructor(parentHtmlElementId, id, data) {
-    super(parentHtmlElementId, id, data)
-
-    this.symbol = this.createElementNS("img")
-    this.data = data
-    this.symbol.src = data
-    this.symbol.setAttribute('id', this.id)
-    this.symbol.draggable = "true"
-    this.parentHtmlElement.appendChild(this.symbol)
-    
-    this.symbol.ondrop = (event) => {
-      event.preventDefault()
-      console.log(event.dataTransfer.getData("text"))
-    }
-
-    this.symbol.ondragstart = (event) => {
-      event.dataTransfer.setData("text/plain", 'Hey this is ImageSymbol')
-    }
-
-
-    this.symbol.ondragover = (event) => {
-      event.preventDefault()
-    }
-
-  }
-}
-
-class Sprite {
-
-  constructor(sheet, cfg) {
-    this.canvas = document.getElementById('myRenderCanvas')
-    this.context = this.canvas.getContext('2d')
-    this.imageSheet = sheet
-    this.cfg = cfg
-  }
-
 }
