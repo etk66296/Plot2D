3609b54e4b77c979a30595bea986ca80314835e7
etk66296 <etk66296@web.de>
2022-06-07 19:50:05 +0200

    testing init cursor styles for single edge stretcher

--- public/dev/spec/container/domSingleDirBorderScaler.js
+++ public/dev/spec/container/domSingleDirBorderScaler.js
@@ -3,7 +3,11 @@ describe("DomSingleDirBorderScaler", function() {
 
   beforeEach(function() {
 
-    myDomSingleDirBorderScaler = new DomSingleDirBorderScaler()
+    myDomSingleDirBorderScaler = new DomSingleDirBorderScaler(
+      document.createElementNS("http://www.w3.org/1999/xhtml", 'div'),
+      'N'
+    )
+
 
   })
 
@@ -44,17 +48,6 @@ describe("DomSingleDirBorderScaler", function() {
 
   describe("init", function() {
 
-    var myDomSingleDirBorderScaler
-
-    beforeEach(function() {
-
-      myDomSingleDirBorderScaler = new DomSingleDirBorderScaler(document.createElementNS(
-        "http://www.w3.org/1999/xhtml",
-        'div'
-      ), 'N')
-  
-    })
-
     it(`should initialize the position attributes x and y, as well
       as the width and height of the scaler`, function() {
 
@@ -67,6 +60,226 @@ describe("DomSingleDirBorderScaler", function() {
           .toEqual(3)
         expect(myDomSingleDirBorderScaler.h)
           .toEqual(4)
+
+      }
+    )
+
+    it("should add an mousemove eventlistener to the document",
+      function() {
+
+        spyOn(document, 'addEventListener')
+        myDomSingleDirBorderScaler.init(1, 2, 3, 4)
+        expect(document.addEventListener).toHaveBeenCalledWith(
+          'mousemove', jasmine.any(Function)
+        )
+
+
+      }
+    )
+
+    it(`should call the super class function stretchParentFromTopBy
+      when the edgeSide is set to 'N' and the attribute mousIsDown
+      is set to true` , function() {
+
+        // let myEvent = { clientY: 100 }
+        // spyOn(myDomSingleDirBorderScaler, 'stretchParentFromTopBy')
+        //   .withArgs(myEvent)
+        // myDomSingleDirBorderScaler.edgeSide = 'N'
+        // myDomSingleDirBorderScaler.mouseIsDown = true
+        // document.dispatchEvent(new Event('mousemove'))
+        // expect(myDomSingleDirBorderScaler.stretchParentFromTopBy)
+        //   .toHaveBeenCalledWith(myEvent)
+        
+
+      }
+    )
+
+    it(`should set its container element the cursor style to
+      'n-resize' when the edge style is set to 'N`, function() {
+
+        myDomSingleDirBorderScaler.init(1, 2, 3, 4)
+        expect(myDomSingleDirBorderScaler
+          .containerElement.style.cursor).toEqual('n-resize')
+
+      }
+    )
+
+    it(`should set its container element the cursor style to
+      's-resize' when the edge style is set to 'S`, function() {
+
+        myDomSingleDirBorderScaler.edgeSide = 'S'
+        myDomSingleDirBorderScaler.init(1, 2, 3, 4)
+        expect(myDomSingleDirBorderScaler
+          .containerElement.style.cursor).toEqual('s-resize')
+
+      }
+    )
+
+    it(`should set its container element the cursor style to
+      'e-resize' when the edge style is set to 'E`, function() {
+
+        myDomSingleDirBorderScaler.edgeSide = 'E'
+        myDomSingleDirBorderScaler.init(1, 2, 3, 4)
+        expect(myDomSingleDirBorderScaler
+          .containerElement.style.cursor).toEqual('e-resize')
+
+      }
+    )
+
+    it(`should set its container element the cursor style to
+      'w-resize' when the edge style is set to 'W`, function() {
+
+        myDomSingleDirBorderScaler.edgeSide = 'W'
+        myDomSingleDirBorderScaler.init(1, 2, 3, 4)
+        expect(myDomSingleDirBorderScaler
+          .containerElement.style.cursor).toEqual('w-resize')
+
+      }
+    )
+
+  })
+
+  it("should have a event function for the added mouse move element",
+    function() {
+      expect(myDomSingleDirBorderScaler.callbackOnMouseMove)
+        .toEqual(jasmine.any(Function))
+    }
+  )
+
+  describe("callbackOnMouseMove", function() {
+
+    var mouseMoveEventObj
+
+    beforeAll(function() {
+      mouseMoveEventObj = {
+
+        preventDefault: function() { }
+
+      }
+    })
+
+    it(`should call the prevent default function of the passed
+      mouse move event object`, function() {
+
+        
+        spyOn(mouseMoveEventObj, "preventDefault")
+        
+        myDomSingleDirBorderScaler.callbackOnMouseMove(
+          mouseMoveEventObj
+        )
+
+        expect(mouseMoveEventObj.preventDefault)
+          .toHaveBeenCalled()
+
+      }
+    )
+
+    it(`should do nothing when the attribute mouseIsDown is set to
+      false`, function() {
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromTopBy')
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromBottomBy')
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromLeftBy')
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromRightBy')
+        myDomSingleDirBorderScaler.mouseIsDown = false
+
+        myDomSingleDirBorderScaler.callbackOnMouseMove(
+          mouseMoveEventObj
+        )
+        expect(myDomSingleDirBorderScaler.stretchParentFromTopBy)
+          .not.toHaveBeenCalled()
+        expect(myDomSingleDirBorderScaler.stretchParentFromBottomBy)
+          .not.toHaveBeenCalled()
+        expect(myDomSingleDirBorderScaler.stretchParentFromLeftBy)
+          .not.toHaveBeenCalled()
+        expect(myDomSingleDirBorderScaler.stretchParentFromRightBy)
+          .not.toHaveBeenCalled()
+
+      }
+    )
+    
+    it(`should do nothing when the attribute mouseIsDown is set but
+      the edgeSide is not set to 'N', 'E', 'S' or 'W'`, function() {
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromTopBy')
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromBottomBy')
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromLeftBy')
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromRightBy')
+        myDomSingleDirBorderScaler.mouseIsDown = true
+        myDomSingleDirBorderScaler.edgeSide = 'H'
+
+
+        myDomSingleDirBorderScaler.callbackOnMouseMove(
+          mouseMoveEventObj
+        )
+        expect(myDomSingleDirBorderScaler.stretchParentFromTopBy)
+          .not.toHaveBeenCalled()
+        expect(myDomSingleDirBorderScaler.stretchParentFromBottomBy)
+          .not.toHaveBeenCalled()
+        expect(myDomSingleDirBorderScaler.stretchParentFromLeftBy)
+          .not.toHaveBeenCalled()
+        expect(myDomSingleDirBorderScaler.stretchParentFromRightBy)
+          .not.toHaveBeenCalled()
+      }
+    )
+
+    it(`should call the super class function stretchParentFromTopBy
+      when the attribute mouseIsDown is true and the edgeSide is
+      set to 'N'`, function() {
+
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromTopBy')
+        myDomSingleDirBorderScaler.mouseIsDown = true
+        myDomSingleDirBorderScaler.callbackOnMouseMove(
+          mouseMoveEventObj
+        )
+        expect(myDomSingleDirBorderScaler.stretchParentFromTopBy)
+          .toHaveBeenCalled()
+
+      }
+    )
+
+    it(`should call the super class function stretchParentFromBottomBy
+      when the attribute mouseIsDown is true and the edgeSide is
+      set to 'S'`, function() {
+
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromBottomBy')
+        myDomSingleDirBorderScaler.mouseIsDown = true
+        myDomSingleDirBorderScaler.edgeSide = 'S'
+        myDomSingleDirBorderScaler.callbackOnMouseMove(
+          mouseMoveEventObj
+        )
+        expect(myDomSingleDirBorderScaler.stretchParentFromBottomBy)
+          .toHaveBeenCalled()
+
+      }
+    )
+
+    it(`should call the super class function stretchParentFromLeftBy
+      when the attribute mouseIsDown is true and the edgeSide is
+      set to 'W'`, function() {
+
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromLeftBy')
+        myDomSingleDirBorderScaler.mouseIsDown = true
+        myDomSingleDirBorderScaler.edgeSide = 'W'
+        myDomSingleDirBorderScaler.callbackOnMouseMove(
+          mouseMoveEventObj
+        )
+        expect(myDomSingleDirBorderScaler.stretchParentFromLeftBy)
+          .toHaveBeenCalled()
+
+      }
+    )
+
+    it(`should call the super class function stretchParentFromRightBy
+      when the attribute mouseIsDown is true and the edgeSide is
+      set to 'E'`, function() {
+
+        spyOn(myDomSingleDirBorderScaler, 'stretchParentFromRightBy')
+        myDomSingleDirBorderScaler.mouseIsDown = true
+        myDomSingleDirBorderScaler.edgeSide = 'E'
+        myDomSingleDirBorderScaler.callbackOnMouseMove(
+          mouseMoveEventObj
+        )
+        expect(myDomSingleDirBorderScaler.stretchParentFromRightBy)
+          .toHaveBeenCalled()
 
       }
     )
